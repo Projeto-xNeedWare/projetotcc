b4ee4d07228c8e2c96beb3e234f82c0829b4f06b
Gabriel Marin Loquetti <126430253+GabrielLoquetti@users.noreply.github.com>
2025-02-28 11:29:40 -0300

    nada

---
+++ pagina_inicial/script.js
@@ -0,0 +1,19 @@
+// Função para adicionar evento de clique em botões
+function addClickEvent(selector, url) {
+    var element = document.querySelector(selector);
+    if (element) {
+      element.addEventListener("click", function () {
+        window.location.href = url;
+      });
+    }
+  }
+
+  // Adicionando eventos aos botões
+  addClickEvent(".boto-produto", "../pagina_produtos/index.html");
+  addClickEvent(".boto-funcionalidades", "../pagina_funcionalidades/index.html");
+  addClickEvent(".boto-assinaturas", "../pagina_assinaturas/index.html");
+  addClickEvent(".boto-contatos", "../pagina_contatos/index.html");
+  addClickEvent(".boto-embreve", "../pagina_em_breve/index.html");
+  addClickEvent(".boto-decadastro", "../pagina_cadastro/index.html");
+  addClickEvent(".text-wrapper-6", "../pagina_inicial/index.html"); // Texto "xNeedWare"
+  addClickEvent(".avatar", "../pagina_login/index.html"); // Ícone de avatar (login)
