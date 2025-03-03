17b18b17389f1f7b077e69b62dd1b3a8d3fc2ae2
carv.dev <166052863+MaCarvalho07@users.noreply.github.com>
2025-03-03 02:50:00 -0300

    Nova pagina de contatos

--- pagina_contatos/script.js
+++ pagina_contatos/script.js
@@ -1,18 +1,21 @@
+// Função para adicionar evento de clique em botões
 function addClickEvent(selector, url) {
     var element = document.querySelector(selector);
     if (element) {
-        element.addEventListener("click", function () {
-            window.location.href = url;
-        });
+      element.addEventListener("click", function () {
+        window.location.href = url;
+      });
     }
-}
+  }
 
-// Adicionando eventos aos botões
-addClickEvent("#botoProdutoContainer", "../pagina_produtos/index.html");
-addClickEvent("#botoFuncionalidadesContainer", "../pagina_funcionalidades/index.html");
-addClickEvent("#botoAssinaturasContainer", "../pagina_assinaturas/index.html");
-addClickEvent("#botoContatosContainer", "../Paginas_Contatos/index.html");
-addClickEvent("#botoEmBreveContainer", "../pagina_em_breve/index.html");
-addClickEvent("#botoDecadastroContainer", "../pagina_cadastro/index.html");
-addClickEvent("#xNeedWareText", "../pagina_inicial/index.html"); // Texto "xNeedWare"
-addClickEvent("#avatar", "../pagina_login/index.html"); // Ícone de avatar (login)
+  // Adicionando eventos aos botões
+  addClickEvent(".boto-produto", "../pagina_produtos/index.html");
+  addClickEvent(".boto-funcionalidades", "../pagina_funcionalidades/index.html");
+  addClickEvent(".boto-assinaturas", "../pagina_assinaturas/index.html");
+  addClickEvent(".boto-contatos", "../pagina_contatos/index.html");
+  addClickEvent(".boto-embreve", "../pagina_em_breve/index.html");
+  addClickEvent(".boto-decadastro", "../pagina_cadastro/index.html");
+  addClickEvent(".text-wrapper-6", "../pagina_inicial/index.html"); // Texto "xNeedWare"
+  addClickEvent(".avatar", "../pagina_login/login.html"); // Ícone de avatar (login)
+  addClickEvent(".text-wrapper-2", "../pagina_produtos/index.html"); // Texto "xNeedWare"
+  addClickEvent(".botosaiba-mais", "../pagina_funcionalidades/index.html");
