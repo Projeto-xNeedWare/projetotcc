c32380ff10b44102163d6697cc6da7b05a8be50f
GabrielLoquetti <126430253+GabrielLoquetti@users.noreply.github.com>
2025-03-05 10:58:23 -0300

    Pagina Cadastro 0.0.2
    Agora na página de cadastro é possível digitar e clicar no botão

--- pagina_cadastro/script.js
+++ pagina_cadastro/script.js
@@ -4,3 +4,14 @@ var lineRoundedCloseIcon = document.getElementById("lineRoundedCloseIcon");
                     window.history.back();
       			});
     		}
+
+			function toggleCheckbox() {
+				let checkbox = document.querySelector(".checkbox");
+				checkbox.classList.toggle("checked");
+			  }
+
+			function fazerCadastro() {
+				alert("Botão de cadastro clicado!");
+				// Redirecionar para outra página
+				// window.location.href = "pagina_de_cadastro.html";
+			  }
