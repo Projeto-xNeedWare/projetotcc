f298f4e850219aa0a7f8e4f45292f3a83567011b
GabrielLoquetti <126430253+GabrielLoquetti@users.noreply.github.com>
2025-04-12 16:48:18 -0300

    adicionando pagina de pagamentos
    adicionei a página de pagamentos e fiz com que todos os produtos e assinaturas levassem até ela

--- pagamento/script.js
+++ pagamento/script.js
@@ -1,7 +1,3 @@
-// A pessoa precisa chegar até esta para voltar para a anterior
-var botaoFecharicon = document.getElementById("botaoFecharicon");
-if (botaoFecharicon) {
-    botaoFecharicon.addEventListener("click", function () {
-        window.history.back(); // Volta para a página anterior no histórico do navegador
-    });
-}
+function voltarPagina() {
+    window.history.back();
+  }
